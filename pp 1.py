f3cbf93e7dfc16c1b53abd74bae51da5006be4c2
ankitha253 <81456396+ankitha253@users.noreply.github.com>
2021-05-20 12:37:15 +0530

    pp1

---
+++ pp 1.py
@@ -0,0 +1,8 @@
+'write a progam to find total and average of 3 numbers'
+a=int(input('enter value'))
+b=int(input('enter value'))
+c=int(input('enter value'))
+sum=a+b+c
+average=sum/3
+print(sum)
+print(average)
