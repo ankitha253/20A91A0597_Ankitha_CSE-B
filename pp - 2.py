7f112c98c66b4a491d0f51bf9adc6754d40daa79
ankitha253 <81456396+ankitha253@users.noreply.github.com>
2021-05-21 11:45:54 +0530

    pp-2

---
+++ pp - 2.py
@@ -0,0 +1,17 @@
+ #unexpected indent
+a=30
+b=20
+print(a+b)
+if a>b:
+     print("a is big")
+else:
+     print("a is small")
+
+
+'''expected output:
+50
+a is big'''
+
+'''actual output:
+50
+a is big'''
