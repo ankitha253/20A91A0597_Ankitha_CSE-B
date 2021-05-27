a12cfcab85fc976ddb996b399dbcb0b600e78ee4
Ankitha.k <81456396+ankitha253@users.noreply.github.com>
2021-05-27 12:56:33 +0530

    x1.py

---
+++ x1.py
@@ -0,0 +1,9 @@
+
+#To read start,end,step_count values from user.
+#By using while loop.
+start=int(input('starting value'))
+end=int(input('ending value'))
+step_count=int(input('step_count'))
+while start<=end:
+    print (start)
+    start+=step_count
