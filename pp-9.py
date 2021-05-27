ae00c03e1809b09e23096f15d466ad4dd0a46760
ankitha253 <81456396+ankitha253@users.noreply.github.com>
2021-05-27 12:36:35 +0530

    pp-9

---
+++ pp-9.py
@@ -0,0 +1,31 @@
+#while loop
+#to print 1 to 10 numbers using while loop.
+i=1
+while(i<=10):
+    print(i)
+    i+=1
+
+
+'''expected output    
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10'''
+
+'''original output
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10'''
